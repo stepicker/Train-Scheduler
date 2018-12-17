4f6aad24847cbe0a80acecc24dce134170b7a330
Picker <picker@gmail.com>
2018-12-17 15:24:31 -0500

    First working version

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Train-Scheduler
+
+Assignment for my Coding Bootcamp at Penn University.
+
+It uses:
+
+- Firebase for data storage
+- Moment.js for time calculations
+- Javascript for the logic
+- jQuery for DOM manipulation
+
+
+## How it works
+
+The user can add trains and include the time when the first one arrives, as well as the frequency.
+
+The app will calculate when the next train is due to arrive from the present time.
+
+
+## Ready to try it?
+
+Head to https://stepicker.github.io/Train-Scheduler
+
+
+## Contributing
+
+Pull requests are always welcome.
---
+++ assets/app.js
@@ -0,0 +1,114 @@
+$(document).ready(function() {
+    
+
+// DATABASE
+// ==================================================
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAWz_zkDyU09Xtu2nLzdw0LpQD3-6pvegg",
+    authDomain: "train-scheduler-e686f.firebaseapp.com",
+    databaseURL: "https://train-scheduler-e686f.firebaseio.com",
+    projectId: "train-scheduler-e686f",
+    storageBucket: "train-scheduler-e686f.appspot.com",
+    messagingSenderId: "373519394339"
+  };
+
+  firebase.initializeApp(config);
+
+
+// VARIABLES
+// ==================================================
+
+// Database
+var database = firebase.database();
+
+// Global variables for user input
+var trainName;
+var trainDestination;
+var trainFirstTime;
+var trainFrequency;
+
+
+// FUNCTIONS FOR LATER USE
+// ==================================================
+
+function msToMinutes(ms) {
+    var minutes = Math.floor(ms / 60000);
+    return minutes;
+}
+
+
+// MAIN PROCESS
+// ==================================================
+
+// Add new trains to the database, based on user input
+$("#train-submit").on("click", function(event) {
+
+    event.preventDefault();
+
+    // Saving user input as local variables
+    trainName = $("#train-name").val().trim();
+    trainDestination = $("#destination").val().trim();
+    trainFirstTime = $("#first-time").val().trim();
+    trainFrequency = $("#frequency").val().trim();
+    
+    // Pushing to the database
+    database.ref().push({
+        trainName: trainName,
+        trainDestination: trainDestination,
+        trainFirstTime: trainFirstTime,
+        trainFrequency: trainFrequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
+
+});
+
+// Get updates from the database, and populate the schedule accordingly
+database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
+
+    // Current Time
+    var currentTime = moment().format("HH:mm");
+    console.log("-----");
+    console.log("Current time: " + currentTime);
+
+    // Storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
+
+    // Logging data received from the database
+    console.log("Train Name: " + sv.trainName);
+    console.log("Destination: " + sv.trainDestination);
+    var firstTime = sv.trainFirstTime;
+    var frequency = sv.trainFrequency;
+    console.log("First Time: " + firstTime);
+    console.log("Frequency: " + frequency);
+
+    // Calculating minutes away
+    var msDifference = moment(currentTime,"HH:mm").diff(moment(firstTime,"HH:mm"));
+    var minDifference = msToMinutes(msDifference);
+    console.log("Time from First Train (in minutes): " + minDifference);
+
+
+    if (minDifference > 0) {
+        var minsAway = (frequency - (minDifference % frequency));
+        var nextTrain = moment(currentTime, "HH:mm").add(minsAway, "minutes").format("HH:mm");
+    }
+
+    else {
+        var minsAway = Math.abs(minDifference);
+        var nextTrain = firstTime;
+    }
+
+    console.log("Next Train: " + nextTrain);
+    console.log("Minutes Away: " + minsAway);
+
+    // Populate the HTML
+    $("#schedule").append("<tr><td>" + sv.trainName + "</td><td>" + sv.trainDestination + "</td><td>" + sv.trainFrequency + "</td><td>" + moment(nextTrain, "HH:mm").format("hh:mm a") + "</td><td>" + minsAway + "</td></tr>");
+
+    // Handle the errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
+
+
+});
